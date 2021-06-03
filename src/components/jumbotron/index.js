048d518f00671bbb822fc8e81b94655fdc1a1d79
siccoo <obichilaka@gmail.com>
2021-06-03 09:26:47 +0100

    updates

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Inner } from "./styles/jumbotron"
 
-export default function Jumbotron () {
+export default function Jumbotron ({ children, direction = "row", ...restProps }) {
     return (
-        <Inner>
+        <Inner direction={direction}>
             <p>Hello again</p>
         </Inner>
     )
