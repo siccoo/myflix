4668f3a60ad9efeb9aac7248612607c298bd714f
siccoo <obichilaka@gmail.com>
2021-06-15 09:59:36 +0100

    updates on jumbotron.js

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Inner } from "./styles/jumbotron"
+import { Container, Inner, Pane, Title, SubTitle, Image } from "./styles/jumbotron"
 
 export default function Jumbotron ({ children, direction = "row", ...restProps }) {
     return (
