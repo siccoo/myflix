5909265267e1a82112eb0eb013f2be04f846a718
siccoo <obichilaka@gmail.com>
2021-06-15 09:23:34 +0100

    Jumbotron func in index.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ export default function App() {
     <Jumbotron.Container>
       {jumboData.map((item) => (
         <Jumbotron key={item.id} direction={item.direction}>
-          <p>{item.id}</p>
-          <p>{item.subTitle}</p>
-          <p>{item.title}</p>
-          <p>{item.image}</p>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
         </Jumbotron>
       ))}
     </Jumbotron.Container>
