1e24b38f21c1fba99cd2ee8c5be13b63ef517a1f
siccoo <obichilaka@gmail.com>
2021-06-15 11:25:53 +0100

    updates on global-styles & index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import GlobalStyles from "./global-styles";
+import { GlobalStyles } from "./global-styles";
 
 render(
   <>
